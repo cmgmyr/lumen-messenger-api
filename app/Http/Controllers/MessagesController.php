d4423c8206f50088599b93ea99fee8f69c0a751a
Chris Gmyr <cmgmyr@gmail.com>
2015-05-01 16:51:34 -0400

    initial update to show() method

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use App\Services\Transformer\MessageTransformer;
 use App\Services\Transformer\ThreadTransformer;
 use App\User;
 use Carbon\Carbon;
@@ -33,7 +34,7 @@ class MessagesController extends ApiController
     /**
      * Show all of the message threads associated with the user
      *
-     * Example URL: /api/messages?api_key=30ce6864e2589b01bc002b03aa6a7923&per_page=10&page=2
+     * Example URL: /api/v1/messages?api_key=30ce6864e2589b01bc002b03aa6a7923&per_page=10&page=2
      *
      * @param ThreadTransformer $threadTransformer
      * @return mixed
@@ -71,29 +72,37 @@ class MessagesController extends ApiController
     /**
      * Shows a message thread
      *
+     * Example URL: /api/v1/messages/1?api_key=30ce6864e2589b01bc002b03aa6a7923
+     *
+     * @param MessageTransformer $messageTransformer
      * @param $id
      * @return mixed
      */
-    public function show($id)
+    public function show(MessageTransformer $messageTransformer, $id)
     {
         try {
             $thread = Thread::findOrFail($id);
         } catch (ModelNotFoundException $e) {
-            Session::flash('error_message', 'The thread with ID: ' . $id . ' was not found.');
-
-            return redirect('messages');
+            return $this->respondNotFound('Sorry, the message thread was not found.');
         }
 
-        // show current user in list if not a current participant
-        // $users = User::whereNotIn('id', $thread->participantsUserIds())->get();
+        $thread->markAsRead($this->user->id);
+        $messages = $thread->messages()->with('user')->get();
 
-        // don't show the current user in list
-        $userId = Auth::user()->id;
-        $users = User::whereNotIn('id', $thread->participantsUserIds($userId))->get();
+        $messageData = $thread->toArray();
+        $messageData['messages'] = $messages->toArray();
 
-        $thread->markAsRead($userId);
+        // @todo: add participants:
+        // $users = User::whereNotIn('id', $thread->participantsUserIds($userId))->get();
+        // @todo: add non-participants
 
-        return view('messenger.show', compact('thread', 'users'));
+        $data = $messageTransformer->transform($messageData);
+
+        $response = [
+            'data' => $data
+        ];
+
+        return $this->respondWithSuccess($response);
     }
 
     /**
